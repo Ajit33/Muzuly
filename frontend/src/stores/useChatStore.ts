4caab24d8252059330dabb5d433eaf1dafddfac8
Ajit33 <justajit33@gmail.com>
2024-12-29 07:48:16 +0530

    "done project"

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -50,62 +50,73 @@ export const useChatStore=create<chatStore>((set,get)=>({
     set({isLoading:false})
   }
   },
-  initSocket:(userId)=>{
-   if(!get().isConnected){
-    socket.auth={userId}
-    socket.connect();
-    socket.emit("user_connected",userId)
-    socket.on("online_Users",(users:string[])=>{
-       set({onlineUsers:new Set(users)})
-    })
-    socket.on("activities",(activities:[string,string][])=>{
-      set({userActivities:new Map(activities)})
-    });
-    socket.on("user_connected",(userId:string)=>{
-      set((state)=>({
-        onlineUsers:new Set([...state.onlineUsers,userId])
-      }))
-    });
-    socket.on("user_disconnected",(userId:string)=>{
-      set((state)=>{
-        const newOnlineusers=new Set(state.onlineUsers);
-        newOnlineusers.delete(userId);
-        return {onlineUsers:newOnlineusers}
-      })
-    });
-    socket.on("receive_message",(message:Message)=>{
-      set((state)=>({
-        messages:[...state.messages,message]
-      }))
-    });
-    socket.on("message_sent",(message:Message)=>{
-      set((state)=>({
-        messages:[...state.messages,message]
-      }))
-    });
-    socket.on("activity_updated",({userId,activity})=>{
-      set((state)=>{
-        const newActivities=new Map(state.userActivities);
-        newActivities.set(userId,activity);
-        return {userActivities:newActivities}
-      })
-    });
-    set({isConnected:true})
-   }
-  },
-  disconnectSocket:()=>{
-    if(get().isConnected){
-      socket.disconnect();
-      set({isConnected:false})
-    }
-    
-  },
-  sendMessage:async (senderId,receiverId,content)=>{
-      const socket=get().socket
-      if(!socket) return
-      socket.emit("send_message",{receiverId,senderId,content})
-  },
-  fetchMessages: async (userId: string) => {
+
+  initSocket: (userId) => {
+		if (!get().isConnected) {
+			socket.auth = { userId };
+			socket.connect();
+
+			socket.emit("user_connected", userId);
+
+			socket.on("users_online", (users: string[]) => {
+				set({ onlineUsers: new Set(users) });
+			});
+
+			socket.on("activities", (activities: [string, string][]) => {
+				set({ userActivities: new Map(activities) });
+			});
+
+			socket.on("user_connected", (userId: string) => {
+				set((state) => ({
+					onlineUsers: new Set([...state.onlineUsers, userId]),
+				}));
+			});
+
+			socket.on("user_disconnected", (userId: string) => {
+				set((state) => {
+					const newOnlineUsers = new Set(state.onlineUsers);
+					newOnlineUsers.delete(userId);
+					return { onlineUsers: newOnlineUsers };
+				});
+			});
+
+			socket.on("receive_message", (message: Message) => {
+				set((state) => ({
+					messages: [...state.messages, message],
+				}));
+			});
+
+			socket.on("message_sent", (message: Message) => {
+				set((state) => ({
+					messages: [...state.messages, message],
+				}));
+			});
+			socket.on("update_activity", ({ userId, activity }) => {
+				set((state) => {
+					const newActivities = new Map(state.userActivities);
+					newActivities.set(userId, activity);
+					return { userActivities: newActivities };
+				});
+			});
+
+			set({ isConnected: true });
+		}
+	},
+
+	disconnectSocket: () => {
+		if (get().isConnected) {
+			socket.disconnect();
+			set({ isConnected: false });
+		}
+	},
+
+	sendMessage: async (receiverId, senderId, content) => {
+		const socket = get().socket;
+		if (!socket) return;
+
+		socket.emit("send_message", { receiverId, senderId, content });
+	},
+	fetchMessages: async (userId: string) => {
 		set({ isLoading: true, error: null });
 		try {
 			const response = await axiosInstance.get(`/users/messages/${userId}`);
@@ -116,5 +127,4 @@ export const useChatStore=create<chatStore>((set,get)=>({
 			set({ isLoading: false });
 		}
 	},
-
-}))
+}));
